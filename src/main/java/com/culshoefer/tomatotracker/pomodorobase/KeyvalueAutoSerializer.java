099eb8516cd318e57c81238110057652adaff0ee
Christoph Ulshoefer <christoph.ulshoefer@yahoo.de>
2016-09-01 22:33:47 -0400

    Added some first code to deal with backing shit to disk

---
+++ src/main/java/com/culshoefer/tomatotracker/pomodorobase/KeyvalueAutoSerializer.java
@@ -0,0 +1,22 @@
+package com.culshoefer.tomatotracker.pomodorobase;
+
+import javax.json.JsonValue;
+
+/**
+ * @author Christoph Ulshoefer <christophsulshoefer@gmail.com> 01/09/16.
+ * This class is responsible for JSON-serializing a configuration given to it.
+ */
+public class KeyvalueAutoSerializer {
+    public void put(String key, JsonValue val){
+        throw new UnsupportedOperationException();
+    }
+    public JsonValue get(String key){
+        throw new UnsupportedOperationException();
+    }
+    public void setToDefault(){
+        throw new UnsupportedOperationException();
+    }
+    public void setToDefault(String key){
+        throw new UnsupportedOperationException();
+    }
+}
